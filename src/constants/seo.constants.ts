947f17d91c1c5137f9b2c49369560ba025b2562a
chicoumbetov <shyngysmaaan@gmail.com>
2025-06-02 20:00:53 +0200

    constants created

---
+++ src/constants/seo.constants.ts
@@ -0,0 +1,5 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+
+export const SITE_NAME = 'MultiEcomShop'
+export const SITE_DESCRIPTION =
+	'Welcome to out internet shop - a unique platform for comfortable and secure shopping, designed with modern trends and technologies in mind.'
