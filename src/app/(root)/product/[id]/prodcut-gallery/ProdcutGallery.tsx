c2ba9b1e69e7adb8c3ed2d1c6b73e51ae58d64d6
chicoumbetov <shyngysmaaan@gmail.com>
2025-09-28 11:15:40 +0200

    :sparkles: ProductGallery comp created

---
+++ src/app/(root)/product/[id]/prodcut-gallery/ProdcutGallery.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import { useState } from 'react'
+
+import { cn } from '@/utils/clsx'
+
+import { IProduct } from '@/shared/domain/entities/product.interface'
+import styles from './ProdcutGallery.module.scss'
+
+interface ProdcutGalleryProps {
+	product: IProduct
+}
+
+export function ProdcutGallery({ product }: ProdcutGalleryProps) {
+	const [currentIndex, setCurrentIndex] = useState(0)
+
+	return (
+		<div>
+			<Image
+				src={product.images[currentIndex]}
+				alt={product.title}
+				width={500}
+				height={500}
+				className={styles.image}
+			/>
+			<div className={styles.gallery}>
+				{product.images.map((image, index) => (
+					<button
+						key={index}
+						onClick={() => setCurrentIndex(index)}
+						className={cn(
+							styles.item,
+							index === currentIndex
+								? 'border-black'
+								: 'border-transparent'
+						)}
+					>
+						<Image
+							src={image}
+							alt={product.title}
+							width={100}
+							height={100}
+						/>
+					</button>
+				))}
+			</div>
+		</div>
+	)
+}
