399f0b92d1e16018b366b12049414a96f39c675e
chicoumbetov <shyngysmaaan@gmail.com>
2025-10-03 13:34:09 +0200

    overview

---
+++ src/app/store/[storeId]/statistics/middle-statistics/Overview.tsx
@@ -0,0 +1,68 @@
+import { Area, AreaChart, CartesianGrid, XAxis } from 'recharts'
+
+
+
+import { formatPrice } from '@/utils/string/format-price'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/presentation/components/ui/Card'
+import { ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/presentation/components/ui/chart'
+import { IMonthlySales } from '@/shared/domain/entities/statistics.interface'
+import styles from './MiddleStatistics.module.scss'
+
+const chartConfig = {
+	value: {
+		label: 'Прибыль',
+		color: '#3B82F6'
+	}
+} satisfies ChartConfig
+
+interface OverviewProps {
+	data: IMonthlySales[]
+}
+
+export function Overview({ data }: OverviewProps) {
+	return (
+		<Card>
+			<CardHeader className={styles.header}>
+				<CardTitle>Прибыль</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<ChartContainer
+					className='aspect-auto h-[310px] w-full'
+					config={chartConfig}
+				>
+					<AreaChart
+						accessibilityLayer
+						data={data}
+						margin={{
+							left: 12,
+							right: 12
+						}}
+					>
+						<CartesianGrid vertical={false} />
+						<XAxis
+							dataKey='date'
+							tickLine={false}
+							axisLine={false}
+							tickMargin={8}
+						/>
+						<ChartTooltip
+							content={
+								<ChartTooltipContent
+									labelFormatter={formatPrice}
+									indicator='line'
+								/>
+							}
+						/>
+						<Area
+							dataKey='value'
+							type='natural'
+							fill='var(--color-value)'
+							stroke='var(--color-value)'
+						/>
+					</AreaChart>
+				</ChartContainer>
+			</CardContent>
+		</Card>
+	)
+}
